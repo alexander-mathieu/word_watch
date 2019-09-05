268e4c48fa5667a2f16f04084b749810dd6a5867
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-05 11:01:26 -0600

    Add top word display to index page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax({
+    url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+    success: function(data) {
+      $('.word-count').append(`The best word is ${Object.keys(data.word)[0]}! That's right, ${Object.keys(data.word)[0]} has a total word count of ${Object.values(data.word)[0]}!`)
+    },
+    error: function(error) {
+      console.log(error)
+    }
+  })
 })
